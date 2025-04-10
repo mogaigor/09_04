8c41c7d855debf119fe4bac47a8238306a12af07
igor moga <moga.igor@iisgalvanimi.edu.it>
2025-04-10 10:39:49 +0000

    s2

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import pandas 
 from flask import Flask,render_template, request, redirect, url_for #importiamo la classe flask
 app = Flask(__name__) #inizializza app flask
-df = pandas.read_csv('profilo.csv')
+
 
 @app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
 def home():
+    df = pandas.read_csv('profilo.csv')
     d1 = df.iloc[0].to_dict()
     return render_template("index.html",df = d1)
 
@@ -14,19 +15,24 @@ def modifica():
     return render_template("modifica.html")
 
 
-@app.route('/aggiungi', methods=['POST'])
+@app.route('/aggiungi', methods=['GET','POST'])
 def aggiungi():
 #ottiene elemento dal form
-    elemento1 = request.form['elemento1']
-    d1["Nome"] = elemento1
-    elemento2 = request.form['elemento2']
-    d1["Cognome"] = elemento2
-
-
-@app.route('/ritorna',methods=['GET','POST'])  #visitiamo (`/`), la funzione home() viene eseguita.
-def ritorna():
+    if request.method == "POST":
+        nuovo_profilo = {
+            "Nome": request.form["elemento1"],
+            "Cognome": request.form["elemento2"],
+            "Scuola": request.form["elemento3"],
+            "Hobby": request.form["elemento4"]
+        }
+        df = pandas.DataFrame([nuovo_profilo])
+        df.to_csv("profilo.csv", index=False)
+        return redirect(url_for('home'))
+    df = pd.read_csv("profilo.csv")
     d1 = df.iloc[0].to_dict()
-    return render_template("index.html",df = d1)
+    return render_template("modifica.html", df=d1)
+
+
 #avvio flask
 if __name__ == '__main__':
     app.run(debug=True) #aggiornamenti in tempo reale
